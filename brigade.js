66da0328a33de5648d2aa4def26e1b3929afd445
Abhishek Chauhan <89007295+Abhii-12@users.noreply.github.com>
2021-10-07 13:52:02 +0530

    Update brigade.js

--- brigade.js
+++ brigade.js
@@ -1,14 +1,23 @@
 const { events, Job } = require("brigadier");
-events.on("exec", () => {
-  var dockerBuild = new Job("docker-build")
+events.on("exec", (e,project) => {
+  var dockerBuild = new Job("docker-packaging");
   dockerBuild.image = "docker:dind";
-
+  dockerBuild.privileged = true;
+  dockerBuild.env = {
+    DOCKER_DRIVER: "overlay",
+    DOCKER_USER: project.secrets.dockerLogin,
+    DOCKER_PASS: project.secrets.dockerPass
+  }
+    
   dockerBuild.tasks = [
     "dockerd-entrypoint.sh &",
     "sleep 30",
-    "cd /src/",
-    "docker build -t Abhii-12/brigade-test:20 ."
-  ]
-
+    "cd /src",
+    "docker build -t abhishekchauhan12/brigade-test:50 .",
+    "docker login -u $DOCKER_USER -p $DOCKER_PASS",
+    "docker push abhishekchauhan12/brigade-test:50"
+  ];
+  
   dockerBuild.run();
 });
+
